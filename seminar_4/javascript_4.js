66cb807fdc163402890cae57ac2ccdf8968cfaa5
popov_oleg <jim_power@mail.ru>
2024-05-21 22:06:59 +0300

    seminar_4

--- seminar_4/javascript_4.js
+++ seminar_4/javascript_4.js
@@ -10,6 +10,17 @@
 третьего - 3.*/
 
 //(1)
+
+// function randomArr(number) {
+//   const arr = [];
+//   for (let i = 0; i < number; i++) {
+//     arr.push(Math.floor(Math.random() * (200 - 100) + 100));
+//   }
+//   return arr;
+// }
+
+// console.log(randomArr(10));
+
 // const array = [];
 // array.push(1);
 // array.push(2);
@@ -90,6 +101,7 @@ const arr = [1, 2, 3];
 //   arrayNum[i]++;
 // }
 // console.log(arrayNum);
+
 //-----------------
 //(2)
 // const arr = [];
@@ -100,17 +112,17 @@ const arr = [1, 2, 3];
 //-----------------
 //(3)
 // const arr = [1, 2, 3];
-// arr.push(4);
-// arr.push(5);
+// arr.push(4,5);
+
 // console.log(arr);
 //------------------------
 //(4)
 // const array = [];
-// array.push(Math.floor(Math.random() * 100));
-// array.push(Math.floor(Math.random() * 100));
-// array.push(Math.floor(Math.random() * 100));
-// array.push(Math.floor(Math.random() * 100));
-// array.push(Math.floor(Math.random() * 100));
+// array.push(10);
+// array.push(65);
+// array.push(18);
+// array.push(71);
+// array.push(11);
 // console.log(array);
 // console.log(`Длина массива - ${array.length}`);
 // const arrayNum = array.slice();
@@ -118,6 +130,44 @@ const arr = [1, 2, 3];
 // arrayNum.pop();
 // console.log(arrayNum);
 // console.log(`Длина массива - ${arrayNum.length}`);
+//или
+//arr.slice(0, arr.length - 2);
+//или
+// function popTwoElems(arrayToProcess) {
+//   for (let x = 0; x < 2; x++) {
+//       arrayToProcess.pop()
+//   }
+//   return arrayToProcess;
+// }
+//или
+// const arr = [1, 2, 3, 4, 5];
+// arr.pop();
+// arr.pop();
+// console.log(arr);
+
+// function deleteTwoEL(array, el){
+//     if(array.length > el){
+//         for (let i = 0; i < el; i++) {
+//             array.pop();
+//         }
+//     }
+
+// }
+
+// const arr = [1, 2, 3];
+// arr.push(4,5);
+// // arr.push(5);
+// console.log(...arr);
+
+// function addEl(array, el, el1){
+//     array.push(el, el1);
+// }
+
+// const arr = [];
+// arr[3] = 'a';
+// arr[8] = 'b';
+
+// console.log(arr.length);
 
 /*Задание 3 (тайминг 25 минут)
 1. С помощью цикла for выведите в консоль числа от 11 до 33.
@@ -134,7 +184,7 @@ const arr = [1, 2, 3];
 //   console.log(index);
 // }
 
-// //или
+//или
 
 // const arrayNum = [];
 // for (let i = 0; i <= 99; i++) {
@@ -195,6 +245,93 @@ const arr = [1, 2, 3];
 // }
 // console.log(count);
 //----------------------------------------------------
+//вариант 2
+/* 1.С помощью цикла for выведите в консоль числа от 11 до 33.*/
+
+// for (let i = 11; i <= 33; i++) {
+//   console.log(i);
+// }
+
+// function printNumbersFrom11to33() {
+//   for (let i = 11; i <= 33; i++) {
+//     console.log(i);
+//   }
+// }
+
+// printNumbersFrom11to33();
+
+/* 2. С помощью цикла for выведите в консоль нечетные числа в промежутке от 1 до 99.*/
+
+// for (let i = 1; i <= 99; i++) {
+//   if (i % 2 !== 0) {
+//     console.log(i);
+//   }
+// }
+
+// function printOddNumbersInRange(start, end) {
+//   for (let i = start; i <= end; i++) {
+//     if (i % 2 !== 0) {
+//       console.log(i);
+//     }
+//   }
+// }
+
+// printOddNumbersInRange(1, 99);
+
+/* С помощью цикла for выведите в консоль числа от 100 до 0.*/
+
+// for (let i = 100; i >= 0; i--) {
+//   console.log(i);
+// }
+/*4. Дано число num с неким начальным значением. Умножайте его на 3 столько раз, пока результат умножения не станет больше 1000. Какое число получится? Посчитайте количество итераций, необходимых для этого.*/
+
+// let num = 1;
+// let iterations = 0;
+
+// while (num <= 1000) {
+//   num *= 3;
+//   iterations++;
+// }
+
+// console.log("Полученное число: " + num);
+// console.log("Количество итераций: " + iterations);
+
+// function multiplyByThreeUntilOver1000(num) {
+//   let result = num;
+//   let iterations = 0;
+
+//   while (result <= 1000) {
+//       result *= 3;
+//       iterations++;
+//   }
+
+//   console.log("Начальное число: " + num);
+//   console.log("Полученное число: " + result);
+//   console.log("Количество итераций: " + iterations);
+// }
+
+// multiplyByThreeUntilOver1000(1);
+//-----------------------------------------------
+// function numbersInRangeBetween(numStart, numEnd) {
+//   let result4 = [];
+//   if (numStart < numEnd) {
+//       for (let index = numStart; index < numEnd; index++) {
+//           result4.push(index);
+//       }
+//   } else if (numStart > numEnd) {
+//       for (let index = numEnd; index > numEnd; index--) {
+//           result4.push(index);
+//       }
+//   } else {
+//       result4.push(numStart);
+//   }
+//   console.log(result4);
+// }
+
+// console.log(numbersInRangeBetween(10, 20));
+// console.log(numbersInRangeBetween(-10, 0));
+// console.log(numbersInRangeBetween(99, 99));
+//-----------------------------------------------------
 /*Задание 4 (тайминг 25 минут)
 1. Дан массив const arr = [2, 5, 9, 15, 1, 4];
 Выведите в консоль те элементы массива, которые больше 3-х, но 
@@ -219,7 +356,12 @@ const arr = [1, 2, 3];
 // let sum = 0;
 // for (let i = 2; i <= 100; i++) {
 //   if (i % 2 === 0) sum += i;
-//   console.log(sum);
+// }
+// console.log(sum);
+
+// let sum = 0;
+// for (let i = 2; i <= 100; i+=2) {
+//   sum += i;
 // }
 // console.log(sum);
 //-----------------------
@@ -231,6 +373,9 @@ const arr = [1, 2, 3];
 //   console.log(sum);
 // }
 // console.log(`Сумма всех чисел массива: ${sum}`);
+
+// console.log(array.reduce((acc, sum) => acc + sum, 10));
+// console.log(array.reduce((acc, sum) => acc * sum));
 //-------------------------------------------
 //(4)
 // let line = "-";
@@ -238,7 +383,12 @@ const arr = [1, 2, 3];
 //   line = line + i + "-";
 // }
 // console.log(`Сформирована строка: ${line}`);
-
+//-----------------
+//let stroka = '-';
+// for (let i = 1; i <= 9; i++) {
+//   stroka += i + '-';
+// }
+// console.log(stroka);
 //(5)
 // let array = [2, 5, 9, 0, 3, 1, 4];
 
@@ -248,7 +398,14 @@ const arr = [1, 2, 3];
 //   }
 //   console.log(array[i]);
 // }
+//
 
+// const array = [2, 5, 9, 0, 3, 1, 4];
+// let i = 0;
+// do {
+//     console.log(array[i]);
+//     i++;
+// } while (array[i] !== 0);
 /*Задание 5 (тайминг 25 минут)
 1. Пусть у нас дан массив состоящий из 10 элементов с 
 произвольными числами. Давайте переберем его циклом и числа, 
@@ -264,23 +421,23 @@ const arr = [1, 2, 3];
 [1, 4, 5]*/
 
 //(1)
-const arrayNum = [];
-for (let i = 0; i < 10; i++) {
-  arrayNum.push(Math.floor(Math.random() * 10));
-}
-console.log(arrayNum);
+// const arrayNum = [];
+// for (let i = 0; i < 10; i++) {
+//   arrayNum.push(Math.floor(Math.random() * 10));
+// }
+// console.log(arrayNum);
 
-for (let i = 0; i < arrayNum.length; i++) {
-  const element = arrayNum[i];
-  if (arrayNum[i] % 2 === 0) {
-    square = arrayNum[i] ** 2;
-    console.log(square);
-  }
-  if (arrayNum[i] % 3 === 0) {
-    cube = arrayNum[i] ** 3;
-    console.log(cube);
-  }
-}
+// for (let i = 0; i < arrayNum.length; i++) {
+//   const element = arrayNum[i];
+//   if (arrayNum[i] % 2 === 0) {
+//     square = arrayNum[i] ** 2;
+//     console.log(square);
+//   }
+//   if (arrayNum[i] % 3 === 0) {
+//     cube = arrayNum[i] ** 3;
+//     console.log(cube);
+//   }
+// }
 
 //-----------------------------------------
 //(2)
